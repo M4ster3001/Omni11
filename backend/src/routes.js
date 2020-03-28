ddc236388f0ce6fac87c4b0811a453e79bfecc5d
Aldo Junior <aldocfjunior@gmail.com>
2020-03-28 11:57:37 -0300

    Alt files

---
+++ backend/src/routes.js
@@ -0,0 +1,26 @@
+const express = require( 'express' );
+
+const routes = express.Router();
+
+//Controllers
+const ongController = require( './controllers/ongController' );
+const incidentsController = require( './controllers/incidentsController' );
+const profileController = require( './controllers/profileController' );
+const sessionController = require( './controllers/sessionController' );
+
+//Session
+routes.get( '/session', sessionController.create );
+
+//Ongs
+routes.get( '/ongs', ongController.index );
+routes.post( '/ongs', ongController.create );
+
+//Profile
+routes.get( '/profile', profileController.index );
+
+//Incidents
+routes.get( '/incidents', incidentsController.index );
+routes.post( '/incidents', incidentsController.create );
+routes.delete( '/incidents/:id', incidentsController.delete );
+
+module.exports = routes; 
